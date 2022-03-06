278f046885b002f52cadd15a4307d3855a112a66
workbritto <workbritto@gmail.com>
2022-03-06 13:24:13 +0530

    initial code

---
+++ src/test/java/com/jenkingit/ThirdProgram.java
@@ -0,0 +1,10 @@
+package com.jenkingit;
+
+public class ThirdProgram {
+	public static void main(String[] args) {
+		String[] s = {" India  is  our         country"};
+		for (String x : s) {
+			System.out.println(x.replaceAll("\\s+", " "));
+		}
+	}
+}
