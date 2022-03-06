27aa914724e51e14b1d1981d4695092f3ec67ba5
workitbritto <90608314+workitbritto@users.noreply.github.com>
2022-03-06 14:40:12 +0530

    Update ThirdProgram.java

--- src/test/java/com/jenkingit/ThirdProgram.java
+++ src/test/java/com/jenkingit/ThirdProgram.java
@@ -6,6 +6,6 @@ public class ThirdProgram {
 		for (String x : s) {
 			System.out.println(x.replaceAll("\\s+", " "));
 		}
-		System.out.println("All the best britto");
+		System.out.println("All the best britto joseph");
 	}
 }
